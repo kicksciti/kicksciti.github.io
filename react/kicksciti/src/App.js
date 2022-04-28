62a32f7ee00d0980b9b166068682514fae86deab
Ben Termux <benjamimeruvieru@gmail.com>
2022-04-28 09:48:20 +0100

    yh

--- react/kicksciti/src/App.js
+++ react/kicksciti/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is the beginning of great things to come, believe me!!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://wa.link/a2hdvs"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Ben Jamine
+          Go to our chat
         </a>
       </header>
     </div>
